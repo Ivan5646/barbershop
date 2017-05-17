a5a925e3700252037d5f511e813840f448dbc898
Ivan5646 <Mike000027@yandex.ru>
2017-05-18 04:53:53 +0700

    gulp livereload

--- gulpfile.js
+++ gulpfile.js
@@ -9,19 +9,29 @@ var uglifycss = require('gulp-uglifycss'); // to minify files
 var concat = require('gulp-concat');
 var minify = require('gulp-minify'); // minify js
 var watch = require('gulp-watch');
+var connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'build',
+    livereload: true
+  });
+});
 
 gulp.task('app-styles', function() {
   return gulp.src("src/styles/blocks/*.scss")
     .pipe(sass().on('error', sass.logError))
     .pipe(uglifycss())
-    .pipe(gulp.dest("build/styles/blocks"));
+    .pipe(gulp.dest("build/styles/blocks"))
+    .pipe(connect.reload());
 });
 
 gulp.task('app-scripts', function() {
   return gulp.src("src/js/*.js")
     .pipe(concat("all.js"))
     .pipe(minify())
-    .pipe(gulp.dest("build/js"));
+    .pipe(gulp.dest("build/js"))
+    .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
@@ -33,3 +43,5 @@ gulp.task('watch', function () {
   gulp.watch('src/js/*.js', appScripts);
   // gulp.watch('src/index.html', index);
 });
+
+gulp.task('default', ['connect', 'watch']);
