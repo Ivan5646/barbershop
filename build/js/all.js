e9a6dd64e1d6585550e98c7cc41ce526d0adbe9f
Ivan5646 <Mike000027@yandex.ru>
2017-05-25 22:59:12 +0700

    js close

--- build/js/all.js
+++ build/js/all.js
@@ -2,5 +2,12 @@ $(document).ready(function(){
   $(".header__hamburger").on("click", function(){
     $(".header__menu").toggle();
   })
+
+  $(".header__close").on("click", function(){
+    $(".header__menu").toggle();
+  })
+
+
+
 });
 console.log("hello from test.js");
--- src/js/common.js
+++ src/js/common.js
@@ -2,4 +2,11 @@ $(document).ready(function(){
   $(".header__hamburger").on("click", function(){
     $(".header__menu").toggle();
   })
+
+  $(".header__close").on("click", function(){
+    $(".header__menu").toggle();
+  })
+
+
+
 });
